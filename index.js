6903a8c2b5496e78bd343bd65f6c7f5d2fe30bf9
MaximBaltak <maksimbaltak@yandex.ru>
2021-04-30 20:43:14 +0200

    Добавил новую функцию

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ let obj={
     name: 'Maxim',
     age: 22,
     status: true,
-    colors:['#a80000','#ffffff','#006ccb']
+    colors:['#a80000','#ffffff','#006ccb'],
+    setValue(age){
+        console.log(this.age+age)
+    }
 }
+obj.setValue(10)
